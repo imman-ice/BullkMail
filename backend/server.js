548391e2ca38e6e72cdc3eee12ab69c04174b6a6
imman-ice <immansamuel52@gmail.com>
2026-05-12 20:26:42 +0530

    Updated transporter for Render

--- backend/server.js
+++ backend/server.js
@@ -37,13 +37,17 @@ app.post("/send-email", async (req, res) => {
   try {
     // transporter
     const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-      }
-    });
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
 
+await transporter.verify();
+console.log("SMTP Verified Successfully");
     // send mail
     await transporter.sendMail({
       from: process.env.EMAIL_USER,
